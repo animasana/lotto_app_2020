a2cfebb635451666bd128d51deb64c8c892c6d2c
Jason Gwon <animagig@gmail.com>
2020-10-03 00:26:28 +0900

    Updates

---
+++ precache-manifest.a22f6696208c316510afd8de9ba6e6ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "162560821573d3f6f8a4884c71bc0815",
+    "url": "/lotto_app_2020/index.html"
+  },
+  {
+    "revision": "1ecf427f22be3800bece",
+    "url": "/lotto_app_2020/static/css/main.fdb3d688.chunk.css"
+  },
+  {
+    "revision": "ae4a5b5343647a834040",
+    "url": "/lotto_app_2020/static/js/2.cf28e21f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lotto_app_2020/static/js/2.cf28e21f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ecf427f22be3800bece",
+    "url": "/lotto_app_2020/static/js/main.019297a6.chunk.js"
+  },
+  {
+    "revision": "d781d4ea9b5677098aaf",
+    "url": "/lotto_app_2020/static/js/runtime-main.3eaefa46.js"
+  }
+]);
