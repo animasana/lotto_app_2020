ab42be14977a4fb1e4824acdcdc24beba55ac533
animasana <41289861+animasana@users.noreply.github.com>
2022-02-10 02:50:46 +0900

    Add files via upload

---
+++ src/LottoReactHooks.js
@@ -0,0 +1,45 @@
+import React from "react";
+import Lotto from "./Lotto";
+import "./LottoReact.css";
+
+function LottoReactHooks(props) {
+	const [lotto, setLotto] = React.useState(new Lotto().toString());
+
+	const strToLines = (str) => {
+		return str.split("\n").map((line, index) => (
+			<span key={"line-" + index}>
+				{line}
+				<br />
+			</span>
+		));
+	};
+
+	const handleClick = (event) => {
+		event.preventDefault();
+		setLotto(lotto.str());
+	};
+
+	const date = new Date();
+	const yyyy = date.getFullYear();
+	const mm = date.getMonth() + 1;
+	const dd = date.getDate();
+
+	return (
+		<React.Fragment>
+			<div className="lottoStyle">
+				<span>
+					{yyyy}년 {mm}월 {dd}일
+				</span>
+				<hr />
+				<span>{props.name}</span>
+				<hr />
+				<span>{strToLines(lotto)}</span>
+			</div>
+			<div className="buttonStyle">
+				<button onClick={handleClick}>로또 재발행</button>
+			</div>
+		</React.Fragment>
+	);
+}
+
+export default LottoReactHooks;
