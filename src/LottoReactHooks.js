0e04daa0fb6ec9def999bdf89d9841839b35da13
animasana <animagig@gmail.com>
2022-02-10 02:38:26 +0900

    2022-02-10

--- src/LottoReactHooks.js
+++ src/LottoReactHooks.js
@@ -1,41 +1,45 @@
-import React from 'react';
-import Lotto from './Lotto';
-import './LottoReact.css';
+import React from "react";
+import Lotto from "./Lotto";
+import "./LottoReact.css";
 
-function LottoReactHooks(props) {    
-    const lotto = new Lotto(5, 6, 45);
-    const [lottoStr, setLottoStr] = React.useState(lotto.str());
-    
-    const strToLines = (str) => {
-        return str.split('\n')
-                  .map((line, index) => <span key={'line-' + index}>{line}<br /></span>);
-    }
+function LottoReactHooks(props) {
+	const [lotto, setLotto] = React.useState(new Lotto().toString());
 
-    const handleClick = (event) => {
-        event.preventDefault();
-        setLottoStr(lotto.str());  
-    }   
+	const strToLines = (str) => {
+		return str.split("\n").map((line, index) => (
+			<span key={"line-" + index}>
+				{line}
+				<br />
+			</span>
+		));
+	};
 
-    const date = new Date();
-    const yyyy = date.getFullYear();
-    const mm   = date.getMonth() + 1;
-    const dd   = date.getDate();
+	const handleClick = (event) => {
+		event.preventDefault();
+		setLotto(lotto.str());
+	};
 
-    return (
-        <React.Fragment>
-            <div className="lottoStyle">
-                <span>{yyyy}년 {mm}월 {dd}일</span>            
-                <hr />
-                <span>{props.name}</span>                                
-                <hr />                    
-                <span>{strToLines(lottoStr)}</span>
-                
-            </div>
-            <div className="buttonStyle">
-                <button onClick={handleClick}>로또 재발행</button>
-            </div>                        
-        </React.Fragment>
-    )
+	const date = new Date();
+	const yyyy = date.getFullYear();
+	const mm = date.getMonth() + 1;
+	const dd = date.getDate();
+
+	return (
+		<React.Fragment>
+			<div className="lottoStyle">
+				<span>
+					{yyyy}년 {mm}월 {dd}일
+				</span>
+				<hr />
+				<span>{props.name}</span>
+				<hr />
+				<span>{strToLines(lotto)}</span>
+			</div>
+			<div className="buttonStyle">
+				<button onClick={handleClick}>로또 재발행</button>
+			</div>
+		</React.Fragment>
+	);
 }
 
 export default LottoReactHooks;
