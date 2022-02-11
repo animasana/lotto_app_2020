8d42274903412a6b6f8a39af6bc62e547d2866c1
animasana <animagig@gmail.com>
2022-02-12 02:56:22 +0900

    final :}

--- src/LottoReact.js
+++ src/LottoReact.js
@@ -1,48 +1,51 @@
-import React from 'react';
-import Lotto from './Lotto';
-import './LottoReact.css';
+import React from "react";
+import Lotto from "./Lotto";
+import "./LottoReact.css";
+
+const date = new Date();
+const yyyy = date.getFullYear();
+const mm = date.getMonth() + 1;
+const dd = date.getDate();
 
 class LottoReact extends React.Component {
-    lotto = new Lotto(5, 6, 45);
+  lottoObj = new Lotto(5, 6, 45);
 
-    state = {                    
-        lottoString: this.lotto.str(),                        
-    };          
+  state = {
+    lotto: this.lottoObj.str(),
+  };
 
-    clickHandler = (e) => {
-        e.preventDefault();
-        this.setState({lottoString: this.lotto.str()});  
-    };
+  clickHandler = (e) => {
+    e.preventDefault();
+    this.setState({ lotto: this.lottoObj.str() });
+  };
 
-    strToLines = (str) => {
-        return str.split('\n').map((line, index) => <span key={'line-' + index}>{line}<br /></span>);
-    }
-    
-    render() {                   
-        const date = new Date();
-        const yyyy = date.getFullYear();
-        const mm   = date.getMonth() + 1;
-        const dd   = date.getDate();
-        
-        const { name } = this.props;
-        const { lottoString } = this.state;
+  strToLines = (str) => {
+    return str.split("\n").map((line, index) => (
+      <span key={"line-" + index}>
+        {line}
+        <br />
+      </span>
+    ));
+  };
 
-        return (                        
-            <React.Fragment>
-                <div className="lottoStyle">
-                    <span>{yyyy}년 {mm}월 {dd}일</span>            
-                    <hr />
-                    <span>{name}</span>                                
-                    <hr />                    
-                    <span>{this.strToLines(lottoString)}</span>
-                    
-                </div>
-                <div className="buttonStyle">
-                    <button onClick={this.clickHandler}>로또 재발행</button>
-                </div>                        
-            </React.Fragment>
-        );        
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <div className="lottoStyle">
+          <span>
+            {yyyy}년 {mm}월 {dd}일
+          </span>
+          <hr />
+          <span>{this.props.name}</span>
+          <hr />
+          <span>{this.strToLines(this.state.lotto)}</span>
+        </div>
+        <div className="buttonStyle">
+          <button onClick={this.clickHandler}>로또 재발행</button>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default LottoReact;
